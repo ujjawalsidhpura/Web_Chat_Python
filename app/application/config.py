51f7be7ccaac8444a6c519d79001038cdc120f88
ujjawal sidhpura <ujjawalsidhpura@gmail.com>
2022-01-14 11:41:43 -0500

    retract .env

--- app/application/config.py
+++ app/application/config.py
@@ -5,3 +5,13 @@ import os
 # Load .ENV
 env_path = Path('.')/'.env'
 load_dotenv(dotenv_path=env_path)
+
+
+class Config:
+    # Set Flask configuration vars from .env file.
+
+    # Load ENV variables
+    TESTING = os.getenv('TESTING')
+    FLASK_DEBUG = os.getenv('FLASK_DEBUG')
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    SERVER = os.getenv('SERVER')
